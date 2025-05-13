05d354597cc566e18afc6a4c7278bdaccdb40fb3
lenny <your_email@example.com>
2025-05-13 14:22:45 +0300

    feat: configure environment variables and integrate dotenv in bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
+import os
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 import asyncio
 
-from genai.items import settings
+from dotenv import load_dotenv
 
+from genai.items import settings
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBAPP_URL = os.getenv("WEBAPP_URL")
 
 
 bot = Bot(
-    token=settings.BOT_TOKEN,
+    token=BOT_TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 dp = Dispatcher()
@@ -17,7 +25,7 @@ dp = Dispatcher()
 @dp.message()
 async def send_webapp_button(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Открыть WebApp", web_app=WebAppInfo(url=settings.WEBAPP_URL))],
+        [InlineKeyboardButton(text="Открыть WebApp", web_app=WebAppInfo(url=WEBAPP_URL))],
     ])
     await message.answer("Нажми кнопку ниже, чтобы открыть приложение", reply_markup=keyboard)
 
